3abca8c4fd850a5bd600573aa9abce7a209c4c74
phitchcock <phitchcock@me.com>
2014-12-03 09:13:12 -0800

    created Event model

---
+++ MeetMeUp/Event.swift
@@ -0,0 +1,14 @@
+//
+//  Event.swift
+//  MeetMeUp
+//
+//  Created by Peter Hitchcock on 12/3/14.
+//  Copyright (c) 2014 Peter Hitchcock. All rights reserved.
+//
+
+import Foundation
+
+
+class Event {
+    var name: String = ""
+}
