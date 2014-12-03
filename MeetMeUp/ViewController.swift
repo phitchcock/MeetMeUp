08c3fa6e6c19734bad3be8b53f33fab13275075d
phitchcock <phitchcock@me.com>
2014-12-03 09:28:54 -0800

    created getData and parseData functions for json

--- MeetMeUp/ViewController.swift
+++ MeetMeUp/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var events = [Event]()
+    let loadURL = "http://s3.amazonaws.com/mobile-makers-assets/app/public/ckeditor_assets/attachments/33/open_events.json"
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,13 +26,50 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return events.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
-        cell.textLabel?.text = "Yep"
+        cell.textLabel?.text = events[indexPath.row].name
         return cell
+    }
+
+    func getData() {
+        let request = NSURLRequest(URL: NSURL(string: loadURL)!)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request, completionHandler: {(data, response, error) -> Void in
+            if error != nil {
+                println(error.localizedDescription)
+            }
+
+            self.events = self.parseData(data)
+            println(self.events)
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tableView.reloadData()
+            })
+        })
+        task.resume()
+    }
+
+    func parseData(data: NSData) -> [Event] {
+        var events = [Event]()
+        var error: NSError?
+
+        let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as? NSDictionary
+
+        if error != nil {
+            println(error?.localizedDescription)
+        }
+
+        let jsonEvents = jsonResult?["results"] as [AnyObject]
+        for jsonEvent in jsonEvents {
+            let event = Event()
+            event.name = jsonEvent["name"] as String
+
+            events.append(event)
+        }
+        return events
     }
 
 
